7d7d80e95a0d9167f9f980573c876f95bc3c0550
Anh Tuấn <anhtuan.bkfet.k55@gmail.com>
2024-01-25 21:59:32 +0700

    Update to support reset wifi manager by hold press button

--- src/SunGTIL-ESP/SunGTIL-ESP.ino
+++ src/SunGTIL-ESP/SunGTIL-ESP.ino
@@ -18,13 +18,15 @@
 #define SERIAL_LOG Serial
 #ifdef ESP32C3_BOARD
 #define SERIAL_GTIL Serial1
-#define RXPIN 6         // GPIO 6 => RX for Serial1
-#define TXPIN 7         // GPIO 7 => TX for Serial1
+#define RXPIN 7  // GPIO 6 => RX for Serial1
+#define TXPIN 6  // GPIO 7 => TX for Serial1
 #define LED_BUILTIN 8
 #else
 #define SERIAL_GTIL Serial2
 #endif
 
+#define TRIGGER_PIN   21  // GPIO 20 => Trigger for reset wifi manager
+#define LOW_MODE_PIN  20 
 /*---> Variables definition*/
 WiFiManager wifiManager;
 
@@ -45,16 +47,26 @@ PubSubClient* mqtt_client;
 /*<---Variables definition*/
 
 
-
-void SendMqttMessage(String msg) {
+void SendMqttErrorLog(String deviceId, String msg) {
   if (!mqtt_client->connected()) {
     mqtt_reconnect();
   }
   mqtt_client->loop();
 
   char topic_publish[128];
-  sprintf(topic_publish, "%s/%s/log/%s", topic_root, device_model, DEVICE_ID);
-  // Serial.println("Publish a message to topic:");
+  sprintf(topic_publish, "%s/%s/debug/%s", topic_root, device_model, deviceId);
+  mqtt_client->publish(topic_publish, msg.c_str());
+}
+
+void SendMqttMetrics(String deviceId, String msg) {
+  if (!mqtt_client->connected()) {
+    mqtt_reconnect();
+  }
+  mqtt_client->loop();
+
+  char topic_publish[128];
+  sprintf(topic_publish, "%s/%s/log/%s", topic_root, device_model, deviceId);
+  // SERIAL_LOG.println("Publish a message to topic:");
   // SERIAL_LOG.println(topic_publish);
   // SERIAL_LOG.println("Message data:");
   // SERIAL_LOG.println(msg);
@@ -76,18 +88,27 @@ void PublishCurrentMetrics(StatusLog status) {
   }
   jLog["dataStreams"] = jStreams;
   // serializeJson(jLog, Serial);
-  // Serial.println("");
+  // SERIAL_LOG.println("");
 
   String jsonStr;
   serializeJson(jLog, jsonStr);
-  SendMqttMessage(jsonStr);
+  SendMqttMetrics(status.deviceGuid, jsonStr);
 }
 
 /////////////////////////////////////////////////////////////////////
 // MODBUS SETUP
 // Handle error from MODBUS client:
 void errorHandler(int errorId, char* errorMessage) {
-  SERIAL_LOG.printf("Error response: %02X - %s\n", errorId, errorMessage);
+  char devId[11];
+  char strLog[128];
+  uint32_t chipId = 0;
+  for (int i = 0; i < 17; i = i + 8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+  }
+  sprintf(devId, "%X", chipId);
+  sprintf(strLog, "Error response: %02X - %s\n", errorId, errorMessage);
+  SERIAL_LOG.printf(strLog);
+  SendMqttErrorLog(devId, strLog);
   blink_led(ERROR_MODE);
 }
 
@@ -95,8 +116,8 @@ void errorHandler(int errorId, char* errorMessage) {
 void dataHandler(GfSun2000Data data) {
   std::map<int16_t, int16_t>::iterator itr;
   for (itr = data.modbusRegistry.begin(); itr != data.modbusRegistry.end(); ++itr) {
-        Serial.printf("Registry %d: %d \n", itr->first, itr->second);
-  }  
+    SERIAL_LOG.printf("Registry %d: %d \n", itr->first, itr->second);
+  }
 
   StatusLog status;
   status.command = CMD_UPDATE_LOG;
@@ -111,13 +132,15 @@ void dataHandler(GfSun2000Data data) {
   PublishCurrentMetrics(status);
 }
 
-void setup_modbus(){
+void setup_modbus() {
 #ifdef ESP32C3_BOARD
   SERIAL_GTIL.begin(9600, SERIAL_8N1, RXPIN, TXPIN);
 #else
   SERIAL_GTIL.begin(9600, SERIAL_8N1);
 #endif
-  while (!SERIAL_LOG) {}
+  while (!SERIAL_GTIL) {
+    delay(10);
+  }
   gtil_device.setup(SERIAL_GTIL);
   gtil_device.setDataHandler(dataHandler);
   gtil_device.setErrorHandler(errorHandler);
@@ -127,11 +150,13 @@ void setup_modbus(){
 // WIFI SETUP
 
 void setup_wifi() {
-  // Reset Wifi settings for testing  
+  digitalWrite(LED_BUILTIN, LOW);
+  // Reset Wifi settings for testing
   //wifiManager.resetSettings();
   wifiManager.setDebugOutput(true);
   wifiManager.setConfigPortalTimeout(30);
   wifiManager.setTimeout(120);
+
 
   //WiFiManagerParameter custom_mqtt_user("username", "Username", mqtt_user, 20);
   //WiFiManagerParameter custom_mqtt_pass("password", "Password", mqtt_password, 20);
@@ -141,35 +166,53 @@ void setup_wifi() {
 
   char strApName[40];
   uint32_t chipId = 0;
-  for(int i=0; i<17; i=i+8) {
-	  chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
-	}
+  for (int i = 0; i < 17; i = i + 8) {
+    chipId |= ((ESP.getEfuseMac() >> (40 - i)) & 0xff) << i;
+  }
   sprintf(strApName, "GTIL2_SUN_WIFI_%X", chipId);
   //wifiManager.autoConnect(strApName);
   if (!wifiManager.autoConnect(strApName)) {
-    Serial.println("WifiManager: failed to connect and hit timeout");
-    delay(3000);
+    SERIAL_LOG.println("WifiManager: failed to connect and hit timeout");
+    delay(5000);
     //reset and try again
     ESP.restart();
   }
-
   //read updated parameters
   //strcpy(mqtt_user, custom_mqtt_user.getValue());
   //strcpy(mqtt_password, custom_mqtt_pass.getValue());
+  SERIAL_LOG.println("WifiManager: Setup WifiManager completed");
+}
 
-  SERIAL_LOG.println("Setup WifiManager completed");
-  blink_led(SUCCESS_MODE);
+
+void check_trigger_button() {
+  if (digitalRead(TRIGGER_PIN) == LOW) {
+    // poor mans debounce/press-hold, code not ideal for production
+    delay(50);
+    if (digitalRead(TRIGGER_PIN) == LOW) {
+      SERIAL_LOG.println("WifiManager: Button Pressed");
+      // still holding button for 3000 ms, reset settings, code not ideaa for production
+      delay(3000);  // reset delay hold
+      if (digitalRead(TRIGGER_PIN) == LOW) {
+        SERIAL_LOG.println("WifiManager: Button Held");
+        SERIAL_LOG.println("WifiManager: Erasing Config, restarting...");
+        wifiManager.resetSettings();
+        ESP.restart();
+        // setup again
+        setup_wifi();
+      }
+    }
+  }
 }
 
 /////////////////////////////////////////////////////////////////////
 // MQTT SETUP
 
 void mqtt_callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
+  SERIAL_LOG.print("Message arrived [");
+  SERIAL_LOG.print(topic);
+  SERIAL_LOG.print("] ");
   for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
+    SERIAL_LOG.print((char)payload[i]);
   }
 }
 
@@ -178,22 +221,22 @@ void mqtt_reconnect() {
   while (!mqtt_client->connected()) {
     // Attempt to connect
     if (mqtt_client->connect(mqtt_client_name, mqtt_user, mqtt_password)) {
-      Serial.println("Mqtt connected");
+      SERIAL_LOG.println("Mqtt connected");
       // ... and resubscribe
       char topic_subscribe[128];
       sprintf(topic_subscribe, "%s/%s/control/%s", topic_root, device_model, DEVICE_ID);
       mqtt_client->subscribe(topic_subscribe);
     } else {
-      Serial.print("Mqtt connect failed, rc=");
-      Serial.print(mqtt_client->state());
-      Serial.println(" try again in 5 seconds");
+      SERIAL_LOG.print("Mqtt connect failed, rc=");
+      SERIAL_LOG.print(mqtt_client->state());
+      SERIAL_LOG.println(" try again in 5 seconds");
       // Wait 5 seconds before retrying
       delay(5000);
     }
   }
 }
 
-void setup_mqtt_client(){
+void setup_mqtt_client() {
   mqtt_client = new PubSubClient(wifiClient);
   mqtt_client->setServer(mqtt_server, 1883);
   mqtt_client->setBufferSize(MQTT_MAX_PACKET_SIZE);
@@ -216,22 +259,33 @@ void blink_led(LED_MODE mode) {
       break;
     case SUCCESS_MODE:
       digitalWrite(LED_BUILTIN, LOW);
-      delay(100);  // wait for a second
+      delay(50);  // wait for a second
       digitalWrite(LED_BUILTIN, HIGH);
       //delay(50);
 
       break;
     case ERROR_MODE:
       digitalWrite(LED_BUILTIN, LOW);
-      delay(500);  // wait for a second
+      delay(1000);  // wait for a second
       digitalWrite(LED_BUILTIN, HIGH);
-      //delay(1000);
+      delay(1000);
       break;
   }
 }
 
 void setup() {
-  WiFi.mode(WIFI_STA); // explicitly set mode, esp defaults to STA+AP
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LOW_MODE_PIN, OUTPUT);
+  pinMode(TRIGGER_PIN, INPUT_PULLUP);
+
+#ifdef ESP32C3_BOARD
+  digitalWrite(LED_BUILTIN, LOW);
+#else
+  digitalWrite(LED_BUILTIN, HIGH);
+#endif
+
+  WiFi.mode(WIFI_STA);  // explicitly set mode, esp defaults to STA+AP
   SERIAL_LOG.begin(9600);
   //Print a message for debug
   SERIAL_LOG.println("SUN GRID-TIE INVERTER 1000/2000");
@@ -241,16 +295,12 @@ void setup() {
   setup_wifi();
   // Setup mqtt client
   setup_mqtt_client();
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
-#ifdef ESP32C3_BOARD
-  digitalWrite(LED_BUILTIN, LOW); 
-#else
-  digitalWrite(LED_BUILTIN, HIGH); 
-#endif
+  
 }
 
 void loop() {
+  digitalWrite(LOW_MODE_PIN, LOW);
+  check_trigger_button();
   gtil_device.readData();
-  delay(900);
+  delay(1000);
 }
